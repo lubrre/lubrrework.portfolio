51bffc07b89f17b2015b48468386fc266d72abfe
lubrre <barrere.lucas@icloud.com>
2022-06-08 00:42:34 +0200

    porsche projet done

--- src/components/Project/Porsche.js
+++ src/components/Project/Porsche.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import "./Styles/OneProject.css";
+import "./Styles/PorscheProject.css";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -11,10 +11,10 @@ import {
 
 function Porsche() {
   return (
-    <div className="OP">
-      <h1 className="OP_title">PORSCHE RENTAL EXPERIENCE</h1>
+    <div className="porsche">
+      <h1 className="porsche_title">PORSCHE RENTAL EXPERIENCE</h1>
 
-      <div className="OP_icon">
+      <div className="porsche_icon">
         <div className="card_icon">
           <FontAwesomeIcon icon={faUser} className="icon"></FontAwesomeIcon>
           <h3 className="icon_title">ROLE</h3>
@@ -37,10 +37,12 @@ function Porsche() {
         </div>
       </div>
 
-      <div className="OP_part1">
-        <h3 className="OP_secondtitle">Contexte</h3>
-        <p className="OP_title_detail">Informations générales</p>
-        <p className="OP_text_p1">
+      <div className="img_porsche_1"></div>
+
+      <div className="porsche_part1">
+        <h3 className="porsche_secondtitle">Contexte</h3>
+        <p className="porsche_title_detail">Informations générales</p>
+        <p className="porsche_text_p1">
           Pour ce projet, le client souhaitait mettre en place une application
           afin de permettre à ses plus fidèles clients de louer certains modèles
           de la concession. Porsche Rental Experience veut offrir à ses clients
@@ -49,12 +51,12 @@ function Porsche() {
         </p>
       </div>
 
-      <div className="OP_company">
+      <div className="porsche_company">
         <div className="company">
           <div className="img_company"></div>
           <div className="company_info">
             <p className="title_company">L'ENTREPRISE</p>
-            <p className="OP_text">
+            <p className="porsche_text">
               Porsche est un constructeur d'automobiles de sport allemand. Fondé
               en 1931 par Ferdinand Porsche, puis repris par son fils Ferry
               Porsche. Ferdinand Porsche est l'ingénieur qui créa la première
@@ -65,10 +67,10 @@ function Porsche() {
         </div>
       </div>
 
-      <div className="OP_brief_challenge">
-        <div className="OP_part1">
-          <p className="OP_title_detail">Le brief</p>
-          <p className="OP_text_p1">
+      <div className="porsche_brief_challenge">
+        <div className="porsche_part1 brief">
+          <p className="porsche_title_detail">Le brief</p>
+          <p className="porsche_text_p1">
             Porsche cherche à avoir une application qui va permettre de
             développer untout nouveau service de location de véhicules pour
             leurs clients Porsche. Pour ce projet, nous avons déjà quelques
@@ -77,38 +79,75 @@ function Porsche() {
           </p>
         </div>
 
-        <div className="OP_part1">
-          <p className="OP_title_detail">Les challenges</p>
-          <p className="OP_text_p1">
-            Porsche cherche à avoir une application qui va permettre de
-            développer untout nouveau service de location de véhicules pour
-            leurs clients Porsche. Pour ce projet, nous avons déjà quelques
-            idées grâce au client. En effet ayant une identité graphique déjà
-            existante cela à permit d'établir un cahier des charges rapidement.
+        <div className="porscherentalexperience"></div>
+      </div>
+
+      <div className="porsche_brief_challenge">
+        <div className="porsche_part1 ">
+          <p className="porsche_secondtitle">
+            Pages principales de l'application
+          </p>
+          <p className="porsche_text mainpages">
+            Les pages principales de l’application, accessibles via la barre de
+            navigation sont : la homepage, le profil, la recherche
           </p>
         </div>
       </div>
+      <div className="porsche_mainpage"></div>
 
-      <div className="OP_part1">
-        <h3 className="OP_secondtitle">Solution</h3>
-        <p className="OP_title_detail">Task flows</p>
-        <p className="OP_text_p1">
-          Comme le projet était complexe et impliquait plusieurs acteurs, l'analyse fonctionnelle réalisé en collaboration avec le client à été extrêment utile. Cela nous permet d'avoir une vision globale des différentes fonctionnalités que nous avons dû concevoir. La création d'un taskflow en mettant en lumière la fonctionnalité principale de l'application permet de comprendre comment l'application va se comporter.
+      <div className="porsche_brief_challenge">
+        <div className="porsche_part1 ">
+          <p className="porsche_secondtitle">
+            On Boarding
+          </p>
+          <p className="porsche_text mainpages">
+            Sign up, Login, walktrhought de l'application permette de se rendre compte des différentes fonctionnalités de cette dernière.
+          </p>
+        </div>
+      </div>      
+      <div className="porsche_onboarding"></div>
+
+
+      <div className="porsche_brief_challenge">
+        <div className="porsche_part1 ">
+          <p className="porsche_secondtitle">
+            User Experience
+          </p>
+          <p className="porsche_text mainpages">
+            La réflexion UX a été centrale pour ce projet qui présente des cibles aux usagers variés tout en étant précises.
+          </p>
+        </div>
+      </div>      
+      <div className="porsche_userflow"></div>
+      <p className="porsche_text userflow">USERFLOW</p>
+
+      <div className="porsche_part1">
+        <h3 className="porsche_secondtitle">Direction artistique</h3>
+        <p className="porsche_title_detail">Branding</p>
+        <p className="porsche_text_p1">
+          Fort de son identité. L'application PORSCHE RENTAL EXPERIENCE reprend les codes graphiques du monde Porsche. La continuité du design repris par l'application est un choix réalisé avec le client afin de rassurer ses clients actuels ainsi que les futurs clients qu'ils sont bien et toujours dans le monde Porsche ou qualité rime toujours avec performance et élégance.
         </p>
-        <div className="OP_userflow1"></div>
-        <p className="OP_text_p2">Userflow de l'utilisateur, montrant le processus pour reserver la location d'un véhicule</p>
-      </div>
-      
-      <div className="OP_part1">
-        <p className="OP_title_detail">Analyse fonctionnelle et maquettes</p>
-        <p className="OP_text_p1">
-          Merci à l'userflow et à l'analyse fonctionnelle, nous étions prêt à créer les maquettes. Différentes features ont été divisés en histoire, chacune d'elle coorespondant à une action de l'utilisateur au sein de l'application. Pour ce projet,les histoires ont pour but de couvrir toutes les actions possibiles pour le client et la concession qui loue le véhicule.
+        <div className="porsche_branding"></div>
+
+        <p className="porsche_title_detail CTA">Pictogrammes, CTA</p>
+        <p className="porsche_text_p1">
+          L'interface de PORSCHE RENTAL EXPERIENCE est sobre et porte une attention particulière aux pictogrammes utilisés. Les boutons Call-To-Action sont aussi étudié de manière à poursuivre la charte graphique du client préalablement établit.
         </p>
-        <p className="OP_text_p3">Cette segmentation en histoire permet:<br></br>-Une cohérence entre l'analyse fontionnelle et les maquettes créés.<br></br>-Une facilité de compréhension lors de la présentaion au client.</p>
-        <div className="OP_wireframes1"></div>
+        <div className="porsche_picto"></div>
+        <p className="porsche_text userflow">PICTOGRAMMES</p>
+        <div className="porsche_CTA"></div>
+        <p className="porsche_text userflow">CALL TO ACTION BUTTON</p>
+
+        <p className="porsche_title_detail CTA">Typographies</p>
+        <p className="porsche_text_p1">
+          Les couleurs de l'application sont issues de la charte graphique de Porsche.
+        </p>
+        <div className="porsche_typo"></div>
+        <p className="porsche_text userflow">TYPOGRAPHIE</p>
+        <div className="porsche_colors"></div>
+        <p className="porsche_text userflow">COULEURS</p>
       </div>
-      
-      
+
     </div>
   );
 }
--- src/components/Project/RaceClub.js
+++ src/components/Project/RaceClub.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import './Styles/OneProject.css'
+// import './Styles/RaceClubProject.css'
 
 function RaceClub() {
   return (
